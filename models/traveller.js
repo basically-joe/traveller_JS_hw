0f0652ba8efc4d7d17576db26dbaf81be6aa4683
Joe McColl <joe4kool@gmail.com>
2019-01-10 09:47:05 +0000

    solved final extension using indexOf, tests passing

--- models/traveller.js
+++ models/traveller.js
@@ -4,48 +4,58 @@ const Traveller = function(journeys) {
 
 Traveller.prototype.getJourneyStartLocations = function() {
   return this.journeys.map((journey) => {
-      return journey.startLocation;
-    });
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
-      return journey.endLocation;
-    });
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-return this.journeys.filter((journey) => {
-  return journey.transport === transport;
-})
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-return this.journeys.filter((journey) => {
-  return journey.distance >= minDistance;
-})
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-return this.journeys.reduce((total, journey) => {
-  return total += journey.distance;
-}, 0)
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
 
-const uniqueTransportTypes = [];
 
-for (const journey of this.journeys) {
-  if (uniqueTransportTypes[journey.transport] === journey.transport) {
-    uniqueTransportTypes[journey.transport] += 0;
-  }
-  else {
-    uniqueTransportTypes[journey.transport] = 1;
-  };
+
+  return this.journeys.reduce((prev, cur) => {
+    if (prev.indexOf(cur.transport) < 0) prev.push(cur.transport);
+        return prev;
+  }, []);
 };
 
-return uniqueTransportTypes;
-};
+  // returns count of unique modes of transport keys
+
+// const uniqueTransportTypes = [];
+//
+// for (const journey of this.journeys) {
+//   if (uniqueTransportTypes[journey.transport] === journey.transport) {
+//     uniqueTransportTypes[journey.transport] += 0;
+//   }
+//   else {
+//     uniqueTransportTypes[journey.transport] = 1;
+//   };
+// };
+//
+// return uniqueTransportTypes;
+
 
 module.exports = Traveller;
